6e16803bd2852b6de0f07d4c7445d77abecc2d73
Ian Kim <ian@funizen.com>
2016-03-22 10:57:16 +0900

    InMobiAndroid5.2.0

---
+++ Assets/Startup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Startup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SceneManager.LoadScene("InMobiTestScene");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
